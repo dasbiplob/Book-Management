a5c59679033aeb4ed09c281a89b99e1964dafe4f
Biplob Das <biplob90.d@gmail.com>
2024-01-30 11:56:04 +0200

    Book Management Service

---
+++ src/components/book-management/BookList.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Book from './Book.svelte';
+  import { useBookStore } from '../../stores/books.svelte.js';
+
+  let selectedBook = $state(null); 
+  
+  const bookStore = useBookStore();
+</script>
+
+<h1>Books</h1>
+
+<ul>
+  {#each bookStore.books as book}
+    <li>{book.name} <button on:click={() => selectedBook = book}>View</button></li>
+  {/each}
+</ul>
+
+{#if selectedBook}
+  <Book book={selectedBook} />
+  <button on:click={() => selectedBook = null}>Clear</button>
+{/if}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { onMount } from 'svelte';
+    import { initBooks } from '../stores/books.svelte.js';
+    import { useBookStore } from "../stores/books.svelte.js";
+    
+    onMount(() => {
+      initBooks(); 
+    });
+  </script>
+  
+  <slot />
